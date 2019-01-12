1d228c3fe162fb934f45933a73c2bd09a43d8e38
jacky <yaacovisraelnathan@gmail.com>
2019-01-12 14:15:04 +0200

    arrenge preview gallary

--- src/app/globals.ts
+++ src/app/globals.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { icon } from '@fortawesome/fontawesome';
 
 @Injectable()
 export class Globals {
@@ -19,4 +20,62 @@ export class Globals {
       viewValue: 'Francais'
     }
   ];
+
+  dialogTextes = [
+    {
+      title: 'easyInstall',
+      hebrewTitle:'התקנה פשוטה',
+      englishTitle:'easy to install',
+      hebrewText:'התקנת המערכת: מערכת EVERHOT מתאימה לכל בית בגלל שלא דורשת תשתית תקשורת חדשה וחיבור לאינטרנט אלא מתלבשת על תשתית חשמל קיימת בעזרת שימוש בטכנולוגיית PLC (Power Line Communication).',
+      iconUrl: 'ICON2.png'
+    },
+    {
+      title: '',
+      hebrewTitle:'',
+      englishTitle:'',
+      hebrewText:'',
+      iconUrl: ''
+    },{
+      title: '',
+      hebrewTitle:'',
+      englishTitle:'',
+      hebrewText:'',
+      iconUrl: ''
+    },{
+      title: '',
+      hebrewTitle:'',
+      englishTitle:'',
+      hebrewText:'',
+      iconUrl: ''
+    },{
+      title: '',
+      hebrewTitle:'',
+      englishTitle:'',
+      hebrewText:'',
+      iconUrl: ''
+    },{
+      title: '',
+      hebrewTitle:'',
+      englishTitle:'',
+      hebrewText:'',
+      iconUrl: ''
+    },{
+      title: '',
+      hebrewTitle:'',
+      englishTitle:'',
+      hebrewText:'',
+      iconUrl: ''
+    },
+  ];
+   index = -1;
+ 
+  filteredObj = function(str){
+    this.dialogTextes.find(function(item, i){
+      if(item.name === str){
+        this.index = i;
+        return i;
+      }
+    });
+    
+  } 
 }
